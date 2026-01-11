03ac087036b0ae3a37083d262774dc564dd891c8
Russell Bulmer <russell.bulmer@gmail.com>
2026-01-11 02:05:21 +0000

    wow 24 cols!

--- smaxc_stack_render_macros.asm
+++ smaxc_stack_render_macros.asm
@@ -1,104 +1,103 @@
 
 
-	MACRO 		Stack_Row_Pixel	   src_num, tar_num							; total 262 T for 20
-																			; 233 T for 18
+	MACRO 		Stack_Row_Pixel	   src_num, tar_num							
+																			
 		; start of sourse row
-		LD 			SP, SCREEN_BASE_src_num								; 10 T
-																			; = 10 T
+		LD 			SP, SCREEN_BASE_src_num								
+																	
 
 		; pop from source to registers
-		POP 		AF 													; 10 T
-		POP 		BC 													; 10 T
-		POP 		DE 													; 10 T
-		POP 		HL													; 10 T
-																			; = 40 T
+		POP 		AF 												
+		POP 		BC 													
+		POP 		DE 												
+		POP 		HL													
+																			
 
 		; flip to shadow registers
-		EXX 															; 4 T
-		EX AF															; 4 T
-																			; = 8 T
-
+		EXX 															
+		EX AF															
+																			
 		; pop from source to shadow registers
-		POP 		AF 													; 10 T
-		POP 		BC 													; 10 T
-		POP 		DE 													; 10 T
-		POP 		HL													; 10 T
-																			; = 40 T
+		POP 		AF 													
+		POP 		BC 													
+		POP 		DE 													
+		POP 		HL													
+																			
 
 		; end of target row
-		LD 			SP, SCREEN_END_tar_num								; 10 T
-																			; = 10 T
+		LD 			SP, SCREEN_END_tar_num								
+																			
 
 		; push from shadow registers to screen
-		PUSH 		HL 													; 11 T
-		PUSH 		DE 													; 11 T
-		PUSH 		BC 													; 11 T
-		PUSH 		AF 													; 11 T
-																			; = 44 T
+		PUSH 		HL 													
+		PUSH 		DE 													
+		PUSH 		BC 													
+		PUSH 		AF 													
+																			
 
 		; flip back to regular registers
-		EXX 															; 4 T
-		EX AF															; 4 T
-																			; = 8 T
+		EXX 															
+		EX AF															
+																			
 
 		; push from registers to screen
-		PUSH 		HL 													; 11 T
-		PUSH 		DE 													; 11 T
-		PUSH 		BC 													; 11 T
-		PUSH 		AF 													; 11 T
-																			; = 44 T
+		PUSH 		HL 													
+		PUSH 		DE 													
+		PUSH 		BC 													
+		PUSH 		AF 													
+																			
 	ENDM 
 
 
 
 
-	MACRO 		Stack_Row_Pixel_Right	   src_num, tar_num				; total 262 T for 20
-																			; 233 T for 18
+	MACRO 		Stack_Row_Pixel_Right	   src_num, tar_num				
+																			
 		; start of sourse row
-		LD 			SP, SCREEN_BASE_RIGHT_src_num								; 10 T
-																			; = 10 T
+		LD 			SP, SCREEN_BASE_RIGHT_src_num								
+																			
 
 		; pop from source to registers
-		POP 		AF 													; 10 T
-		; POP 		BC 													; 10 T
-		; POP 		DE 													; 10 T
-		; POP 		HL													; 10 T
-		; 																	; = 40 T
+		POP 		AF 													
+		POP 		BC 													
+		POP 		DE 													
+		POP 		HL													
+		; 																	
 
 		; ; flip to shadow registers
-		; EXX 															; 4 T
-		; EX AF															; 4 T
-		; 																	; = 8 T
+		;EXX 															
+		; EX AF															
+		; 																	
 
 		; ; pop from source to shadow registers
-		; POP 		AF 													; 10 T
-		; POP 		BC 													; 10 T
-		; POP 		DE 													; 10 T
-		; POP 		HL													; 10 T
-																			; = 40 T
+		; POP 		AF 													
+		; POP 		BC 													
+		; POP 		DE 													
+		; POP 		HL													
+																			
 
 		; end of target row
-		LD 			SP, SCREEN_END_RIGHT_tar_num								; 10 T
-																			; = 10 T
+		LD 			SP, SCREEN_END_RIGHT_tar_num								
+																			
 
 		; ; push from shadow registers to screen
-		; PUSH 		HL 													; 11 T
-		; PUSH 		DE 													; 11 T
-		; PUSH 		BC 													; 11 T
-		; PUSH 		AF 													; 11 T
-		; 																	; = 44 T
+		; PUSH 		HL 													
+		; PUSH 		DE 													
+		; PUSH 		BC 													
+		; PUSH 		AF 													
+		; 																	
 
 		; ; flip back to regular registers
-		; EXX 															; 4 T
-		; EX AF															; 4 T
-		; 																	; = 8 T
+		;EXX 															
+		; EX AF															
+		; 																	
 
 		; ; push from registers to screen
-		; PUSH 		HL 													; 11 T
-		; PUSH 		DE 													; 11 T
-		; PUSH 		BC 													; 11 T
-		PUSH 		AF 													; 11 T
-																			; = 44 T
+		PUSH 		HL 													
+		PUSH 		DE 													
+		PUSH 		BC 													
+		PUSH 		AF 													
+																			
 	ENDM 
 
 
